256e8da36d6f2b91aa4d856514b75a107521f286
Tieugiagia <phantuankiet3008@gmail.com>
2026-06-27 18:30:33 +0700

    Use HashRouter

--- web_client/src/App.js
+++ web_client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom' 
+import { HashRouter as Router, Routes, Route } from 'react-router-dom' 
 import Home from './Pages/Home';
 import ChooseQuiz from './Pages/ChooseQuiz';
 import QuizMajor from './Pages/QuizMajor';
